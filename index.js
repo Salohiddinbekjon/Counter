e1dc10cfa9f2a24723536fbf8ff0549c4fdb03f3
Salohidddin Axmadjonov <axmadjonovsalohidddin97@gmail.com>
2025-01-24 16:17:24 +0500

    Tayor

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 let minus = document.querySelector(".minus")
 let plus = document.querySelector(".plus")
 let span = document.querySelector("span")
+let res = document.querySelector(".res")
+let number = prompt("Bitta kilik nechi baravar bo'lsin")
 
 plus.addEventListener("click", () =>{
-    span.textContent = Number(span.textContent) +1;
+    span.textContent = Number(span.textContent) + Number(number);
 })
 
-minus.addEventListener("click", () => {
+res.addEventListener("click", () => {
     span.textContent = Number(span.textContent=== 0)
 })
+
+minus.addEventListener("click", () =>{
+    span.textContent = Number(span.textContent) -1;
+})
+
+
